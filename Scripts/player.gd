58adde49093931c1893ac9cae7bb67d8ca85c6d0
Kai-Campbell <kaimcampbell2002@gmail.com>
2026-04-23 23:05:15 -0400

    Added Golf, Bowling, Soccer, Basketball levels and fixed player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,13 @@
 extends RigidBody3D
-@export var moving_force = 30
+@export var moving_force = 400
 @export var jump_force = 10
-var camera_speed = 10
+@export var friction = 1.2
+@export var air_movement_speed = 10
 
 func _ready():
 	pass
 
 func _physics_process(delta: float) -> void:
-	
 	if Input.is_action_pressed("forward"):
 		angular_velocity.x -= moving_force * delta
 	if Input.is_action_pressed("backward"):
@@ -18,6 +18,20 @@ func _physics_process(delta: float) -> void:
 		angular_velocity.z -= moving_force * delta
 		
 	if Input.is_action_just_pressed("jump"):
-		linear_velocity.y += 10
+		linear_velocity.y += 40
+		
+	angular_velocity /= friction
 	
-	$CameraRig.position = lerp($CameraRig.position, position, camera_speed * delta)
+	linear_velocity += get_gravity() * delta
+	
+	if global_position.y >= 20:
+		if Input.is_action_pressed("forward"):
+			global_position.z -= air_movement_speed * delta
+		if Input.is_action_pressed("backward"):
+			global_position.z += air_movement_speed * delta
+		if Input.is_action_pressed("left"):
+			global_position.x -= air_movement_speed * delta
+		if Input.is_action_pressed("right"):
+			global_position.x += air_movement_speed * delta
+	
+	
